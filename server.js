d406be5149d0e551992544837ef50eed4df7ac78
Robert de Almeida Bonfada <robert@localhost.localdomain>
2018-01-23 22:46:07 -0200

    That's works out

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+const request = require('request');
+const apiKey = 'd0370348e885c20d760faa56f6a59129'
+
+// ...
+// ...
+
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index')
+})
+
+app.post('/', function (req, res) {
+	let city = request.body.city;
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+	
+	
+	request(url, function (err, response, body) {
+	    if(err){
+		res.render('index', {weather: null, error: 'Erro, please try again'});
+	    }else {
+		let weather = JSON.parse(body)
+		if(weather.main == undefined) {
+	           res.render('index', {weather: null, error: 'Error, please try again'});
+		} else {
+		   let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+		   res.render('index', {weather: weatherText, error: null});
+		}
+	}
+	})
 })
 
 app.listen(3000, function () {
